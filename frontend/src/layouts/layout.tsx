f0c2f84c652de6a155831a8194f3d300ec3ba189
mr-vsr <vikasraivsr630@gmail.com>
2024-03-31 13:39:31 +0530

    done with the landing page

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react"
-import Header  from "../components/Header"
+import Header from "../components/Header"
+import Hero from "../components/Hero"
+import Footer from "../components/Footer"
 
 type Props = {
     children: React.ReactNode;
@@ -9,7 +11,9 @@ const Layout = ({ children}: Props) => {
     return (
         <div className="flex flex-col min-h-screen" >
             <Header />
+            <Hero />
             <div className="container mx-auto flex-1 py-10">{children}</div>
+            <Footer/>
         </div>
     )
 }
