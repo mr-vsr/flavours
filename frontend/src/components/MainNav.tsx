13d5ae2e1178d8d820fc1c6dd761cd4a5ea53f5a
mr-vsr <vikasraivsr630@gmail.com>
2024-03-31 15:36:40 +0530

    added auth0 for authorization and login signup

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,8 +1,16 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 export default function MainNav() {
-return (
-    <Button variant="ghost" className="font-bold hover:text-orange-500 hover:bg-white">
+
+    const { loginWithRedirect } = useAuth0();
+
+    return (
+        <Button
+            variant="ghost"
+            className="font-bold hover:text-orange-500 hover:bg-white"
+            onClick={async () => await loginWithRedirect()}
+        >
         Log In
     </Button>
 )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './global.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
+import Auth0ProviderWithNavigat from './auth/Auth0ProviderWithNavigat'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProviderWithNavigat>
+        <AppRoutes />
+      </Auth0ProviderWithNavigat>
     </Router>
   </React.StrictMode>,
 )
