023205732a9b5fa2536ce90bacf6627c868e5206
mr-vsr <vikasraivsr630@gmail.com>
2024-03-31 13:11:34 +0530

    home page header done with mobile nav

---
+++ frontend/src/components/MobileNav.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+    Sheet,
+    SheetTitle,
+    SheetTrigger,
+    SheetContent,
+    SheetDescription,
+} from './ui/sheet'
+import { Separator } from './ui/separator'
+import { Button } from './ui/button'
+import { Menu } from 'lucide-react'
+
+export default function MobileNav() {
+  return (
+      <Sheet>
+          <SheetTrigger>
+              <Menu className= "text-orange-500"/>
+          </SheetTrigger>
+          <SheetContent className='space-y-3'>
+              <SheetTitle>
+                  <span>Welcome to Flavours!</span>
+              </SheetTitle>
+              <Separator />
+              <SheetDescription className='flex'>
+                  <Button className='flex-1 font-bold bg-orange-500'>Log In</Button>
+              </SheetDescription>
+          </SheetContent>
+    </Sheet>
+  )
+}
